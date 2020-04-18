49eccb49588b1621608710cff3f50a11908a3982
shady-chen <710254315@qq.com>
2020-04-18 17:03:26 +0800

    online

--- application/database.php
+++ application/database.php
@@ -17,9 +17,9 @@ return [
     // 数据库名
     'database'        => 'ordersdb',
     // 用户名
-    'username'        => 'root',
+    'username'        => 'ordersdb',
     // 密码
-    'password'        => 'root',
+    'password'        => 'ordersdb',
     // 端口
     'hostport'        => '',
     // 连接dsn
