0a1fb2f6d4dad2a17d600c9345c42eb52f85e5cf
shady-chen <710254315@qq.com>
2020-04-18 17:00:39 +0800

    fixed the download count and no.site

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -142,7 +142,6 @@ class Admin extends Base
         $return_data = [];
         $website_list = file_get_contents("http://us.shopnm.top/showDir.php");
         $website_list = json_decode($website_list, true);
-        $count = count($website_list);
         $insertCount = 0;
         foreach ($website_list as $key=> $value)
         {
@@ -162,8 +161,6 @@ class Admin extends Base
                 'new'=>0,
             ];
         }
-        $existCount= $count-$insertCount;
-
 
         $file_count = 0;
         $file_insert_count = 0;
@@ -171,28 +168,26 @@ class Admin extends Base
         {
             $file_list = file_get_contents("http://us.shopnm.top/showFiles.php?dir=/".substr($value,11));
             $file_list = json_decode($file_list, true);
-
-            $per_website_count = 0;
             $return_data[$key]['count'] = count($file_list);
             foreach ($file_list as $k=>$v)
             {
-
                 $number = 11+strlen(substr($value,11))+1;
                 if(!db('file_name')->where(['file_name'=>substr($v,$number)])->find())
                 {
                     db('file_name')->insert(['file_name'=>substr($v,$number),"belong"=>substr($value,11),"create_time"=>time()]);
                     $file_insert_count++;
-                    $return_data[$key]['new'] = $return_data[$key]['new']+1;
-                }
-                else
-                {
-                    $return_data[$key]['downloaded'] = $return_data[$key]['downloaded']+1;
                 }
                 $file_count++;
-
             }
         }
-        $file_exist_count = $file_count - $file_insert_count;
+        //数据库中查看已下载种未下载的数量
+        for($i=0;$i<count($return_data);$i++)
+        {
+            $return_data[$i]['count'] = db('file_name')->where(['belong'=>$return_data[$i]['domain'] ])->count();
+            $return_data[$i]['downloaded'] = db('file_name')->where(['belong'=>$return_data[$i]['domain'],'collected'=>1])->count();
+            $return_data[$i]['new'] = db('file_name')->where(['belong'=>$return_data[$i]['domain'],'collected'=>0])->count();
+        }
+
         return json($return_data);
         //到此目录已完成
     }
